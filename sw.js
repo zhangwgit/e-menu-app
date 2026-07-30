fde03a053a3e92882221ed4d1556cece09eefe10
zhangwgit <1191286064@qq.com>
2026-07-30 13:59:37 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Service Worker for e-menu PWA
+const CACHE_NAME = 'emenu-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './admin.html',
+  './style.css',
+  './app.js',
+  './admin.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
